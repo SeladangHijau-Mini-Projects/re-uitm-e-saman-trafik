4efc7df5da29b3fd5ad4ac15e94f1009d3f269ba
Muhammad Nadzmi Bin Mohamed Idzham <nadzmiidzham@gmail.com>
2021-03-14 00:06:04 +0800

    added is not empty to required fields upon create user

--- src/app/user/dto/create-user.dto.ts
+++ src/app/user/dto/create-user.dto.ts
@@ -1,23 +1,33 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsString, IsNumberString, IsOptional } from 'class-validator';
+import {
+    IsEnum,
+    IsString,
+    IsNumberString,
+    IsOptional,
+    IsNotEmpty,
+} from 'class-validator';
 import { UserRank } from '../enum/user-rank.enum';
 import { UserType } from '../enum/user-type.enum';
 
 export class CreateUserDto {
     @ApiProperty({ description: 'User rank', enum: UserRank, example: 'ur1' })
     @IsEnum(UserRank)
+    @IsNotEmpty()
     readonly rank: string;
 
     @ApiProperty({ description: 'User type', enum: UserType, example: 'admin' })
     @IsEnum(UserType)
+    @IsNotEmpty()
     readonly type: string;
 
     @ApiProperty({ description: 'User staff code', example: 'K380' })
     @IsString()
+    @IsNotEmpty()
     readonly userCode: string;
 
     @ApiProperty({ description: 'User full name', example: 'John Doe' })
     @IsString()
+    @IsNotEmpty()
     readonly fullname: string;
 
     @ApiProperty({ description: 'User phone tel no', example: '0111234567' })
@@ -27,5 +37,6 @@ export class CreateUserDto {
 
     @ApiProperty({ description: 'User office tel no', example: '044911234' })
     @IsNumberString()
+    @IsNotEmpty()
     readonly officeTelNo: string;
 }
