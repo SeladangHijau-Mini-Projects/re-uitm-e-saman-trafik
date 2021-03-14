72cc5198c53e1898b0794627f6aaf7b69dce2ef4
Muhammad Nadzmi Bin Mohamed Idzham <nadzmiidzham@gmail.com>
2021-03-14 21:10:51 +0800

    added register endpoint

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { ExistsException } from 'src/common/exception/exists.exception';
+import { CreateUserDto } from '../user/dto/create-user.dto';
 import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
 import { LoggedInDto } from './dto/logged-in.dto';
 import { LoginDto } from './dto/login.dto';
+import { RegisteredDto } from './dto/registered.dto';
 
 @Controller()
 export class AuthController {
@@ -27,5 +30,25 @@ export class AuthController {
         return {
             userToken,
         } as LoggedInDto;
+    }
+
+    @Post('register')
+    async register(@Body() body: CreateUserDto): Promise<RegisteredDto> {
+        const existingUser = await this.userService.findOneByUserCode(
+            body.userCode,
+        );
+
+        if (existingUser) {
+            throw new ExistsException('User code exist.');
+        }
+
+        const newUser = await this.userService.create(body);
+        const resetPasswordUrl = `${process.env.HOST}/forgot-password/${newUser.id}`;
+
+        return {
+            id: newUser.id,
+            username: newUser.userCode,
+            resetPasswordUrl,
+        } as RegisteredDto;
     }
 }
