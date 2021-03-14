8d4679510d8e6275b3060fb1b524ef25b754d2ec
Muhammad Nadzmi Bin Mohamed Idzham <nadzmiidzham@gmail.com>
2021-03-14 21:35:08 +0800

    added forgot password endpoint

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Param,
+    ParseIntPipe,
+    Post,
+    UnauthorizedException,
+} from '@nestjs/common';
 import { ExistsException } from 'src/common/exception/exists.exception';
+import { InvalidValueException } from 'src/common/exception/invalid-value.exception';
 import { CreateUserDto } from '../user/dto/create-user.dto';
+import { UpdateUserDto } from '../user/dto/update-user.dto';
 import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
 import { LoggedInDto } from './dto/logged-in.dto';
 import { LoginDto } from './dto/login.dto';
+import { PasswordResetDto } from './dto/password-reset.dto';
 import { RegisteredDto } from './dto/registered.dto';
 
 @Controller()
@@ -43,12 +53,43 @@ export class AuthController {
         }
 
         const newUser = await this.userService.create(body);
-        const resetPasswordUrl = `${process.env.HOST}/forgot-password/${newUser.id}`;
+        const resetPasswordUrl = `${process.env.HOST}/forgot-password/${newUser.id}`; // TODO: need to generate a correct URL for reset password
 
         return {
             id: newUser.id,
             username: newUser.userCode,
             resetPasswordUrl,
         } as RegisteredDto;
+    }
+
+    @Post('forgot-password/:userId')
+    async forgotPassword(
+        @Param('userId', ParseIntPipe) userId: number,
+        @Body() body: PasswordResetDto,
+    ): Promise<LoggedInDto> {
+        const user = await this.userService.findOne(userId);
+
+        if (body.password != body.confirmPassword) {
+            throw new InvalidValueException(
+                `Both password & confirmPassword field is not equal.`,
+            );
+        }
+        if (user.password == body.password) {
+            throw new InvalidValueException(
+                'Password must not be the same as previously set.',
+            );
+        }
+
+        const updatedUser = await this.userService.update(user, {
+            password: body.password,
+        } as UpdateUserDto);
+        const userToken = this.authService.generateToken(
+            updatedUser.id,
+            updatedUser.userType.name,
+        );
+
+        return {
+            userToken,
+        } as LoggedInDto;
     }
 }
