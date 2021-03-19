56d501a5e7b4c8f472da060c28c092975c5b95ce
Muhammad Nadzmi Bin Mohamed Idzham <nadzmiidzham@gmail.com>
2021-03-19 23:55:43 +0800

    traffic error module

--- src/app/traffic-error/traffic-error.module.ts
+++ src/app/traffic-error/traffic-error.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReportTrafficErrorEntity } from '../report/repository/report-traffic-error.entity';
 import { TrafficErrorEntity } from './repository/traffic-error.entity';
 import { TrafficErrorService } from './traffic-error.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([TrafficErrorEntity])],
+    imports: [
+        TypeOrmModule.forFeature([
+            TrafficErrorEntity,
+            ReportTrafficErrorEntity,
+        ]),
+    ],
     providers: [TrafficErrorService],
     exports: [TrafficErrorService],
 })
--- src/app/traffic-error/traffic-error.service.ts
+++ src/app/traffic-error/traffic-error.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { ReportTrafficErrorEntity } from '../report/repository/report-traffic-error.entity';
 import { TrafficErrorEntity } from './repository/traffic-error.entity';
 
 @Injectable()
@@ -8,5 +9,31 @@ export class TrafficErrorService {
     constructor(
         @InjectRepository(TrafficErrorEntity)
         private readonly trafficErrorRepository: Repository<TrafficErrorEntity>,
+        @InjectRepository(ReportTrafficErrorEntity)
+        private readonly reportTrafficErrorRepository: Repository<
+            ReportTrafficErrorEntity
+        >,
     ) {}
+
+    async createReportTrafficError(
+        reportId: number,
+        errorNameList: string[],
+    ): Promise<TrafficErrorEntity[]> {
+        const trafficErrorList = [];
+
+        for await (const name of errorNameList) {
+            const trafficError = await this.trafficErrorRepository.findOne({
+                name,
+            });
+
+            await this.reportTrafficErrorRepository.save({
+                reportId,
+                trafficError,
+            } as ReportTrafficErrorEntity);
+
+            trafficErrorList.push(trafficError);
+        }
+
+        return trafficErrorList;
+    }
 }
