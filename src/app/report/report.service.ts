77dacf237cffb559af68f4f23a0fd99987fc4af0
Muhammad Nadzmi Bin Mohamed Idzham <nadzmiidzham@gmail.com>
2021-03-19 23:56:52 +0800

    updated report service

--- src/app/report/report.service.ts
+++ src/app/report/report.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { StudentService } from '../student/student.service';
-import { TrafficErrorService } from '../traffic-error/traffic-error.service';
-import { TransportService } from '../transport/transport.service';
 import { CreateReportDto } from './dto/create-report.dto';
 import { UpdateReportDto } from './dto/update-report.dto';
 import { ReportHistoryEntity } from './repository/report-history.entity';
@@ -21,14 +18,43 @@ export class ReportService {
         >,
         @InjectRepository(ReportStatusEntity)
         private readonly reportStatusRepository: Repository<ReportStatusEntity>,
-        private readonly studentService: StudentService,
-        private readonly transportService: TransportService,
-        private readonly trafficErrorService: TrafficErrorService,
     ) {}
 
     async create(dto: CreateReportDto): Promise<ReportEntity> {
-        console.log(await dto);
-        return null;
+        const status = await this.reportStatusRepository.findOne({
+            name: dto.status,
+        });
+
+        // create new report
+        let report = await this.reportRepository.save({
+            status,
+            transportId: dto.transportId,
+            userId: dto.userId,
+            studentId: dto.studentId,
+            location: dto.location,
+        } as ReportEntity);
+
+        // create new report history
+        let reportHistory = null;
+        if (report) {
+            reportHistory = await this.reportHistoryRepository.save({
+                reportId: report.id,
+                statusId: report.statusId,
+                userId: report.userId,
+                transportId: report.transportId,
+                location: report.location,
+                remark: dto.remark,
+            } as ReportHistoryEntity);
+        }
+
+        if (reportHistory) {
+            report = {
+                ...report,
+                histories: [reportHistory],
+            } as ReportEntity;
+        }
+
+        return report;
     }
 
     async update(dto: UpdateReportDto): Promise<ReportEntity> {
