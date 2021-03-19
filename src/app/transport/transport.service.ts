48e5d183cc43d52b587974c6934357b31e5e9f86
Muhammad Nadzmi Bin Mohamed Idzham <nadzmiidzham@gmail.com>
2021-03-19 23:56:28 +0800

    updated transport service

--- src/app/transport/transport.service.ts
+++ src/app/transport/transport.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateTransportDto } from './dto/create-transport.dto';
 import { TransportStatusEntity } from './repository/transport-status.entity';
 import { TransportTypeEntity } from './repository/transport-type.entity';
 import { TransportEntity } from './repository/transport.entity';
@@ -19,4 +20,25 @@ export class TransportService {
             TransportStatusEntity
         >,
     ) {}
+
+    async findOneByPlateNo(plateNo: string): Promise<TransportEntity> {
+        return this.transportRepository.findOne({ plateNo });
+    }
+
+    async create(dto: CreateTransportDto): Promise<TransportEntity> {
+        const type = await this.transportTypeRepository.findOne({
+            name: dto.type,
+        });
+        const status = await this.transportStatusRepository.findOne({
+            name: dto.status,
+        });
+
+        return this.transportRepository.save({
+            plateNo: dto.plateNo,
+            passCode: dto.passCode,
+            studentId: dto.studentId,
+            transportType: type,
+            transportStatus: status,
+        } as TransportEntity);
+    }
 }
