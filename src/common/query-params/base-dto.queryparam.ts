c67680eec4625289e786ec560eb242c8d4abca9b
Muhammad Nadzmi Bin Mohamed Idzham <nadzmiidzham@gmail.com>
2021-03-28 22:33:43 +0800

    added query filter & query param for report

--- src/common/query-params/base-dto.queryparam.ts
+++ src/common/query-params/base-dto.queryparam.ts
@@ -1,36 +1,44 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export abstract class BaseQueryParamDTO {
     @ApiProperty()
+    @IsNumber()
     @IsOptional()
-    id: string;
+    id: number;
 
     @ApiProperty()
+    @IsNumber()
     @IsOptional()
     page: number = 1;
 
     @ApiProperty()
+    @IsNumber()
     @IsOptional()
     limit: number = 30;
 
     @ApiProperty()
+    @IsString()
     @IsOptional()
     orderBy: string = '-id';
 
     @ApiProperty()
+    @IsString()
     @IsOptional()
     fields: string;
 
     @ApiProperty()
+    @IsString()
     @IsOptional()
     from: string;
 
     @ApiProperty()
+    @IsString()
     @IsOptional()
     to: string;
 
     @ApiProperty()
+    @IsString()
     @IsOptional()
     paginationMeta: string;
 }
