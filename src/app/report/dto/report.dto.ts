329f3c8505ebb2291062d36d79e2956b665a3197
Muhammad Nadzmi Bin Mohamed Idzham <nadzmi.idzham@carsome.com>
2021-04-25 15:04:48 +0800

    changed value for enum to use the key values

--- src/app/report/dto/report.dto.ts
+++ src/app/report/dto/report.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDate, IsNumber, IsString } from 'class-validator';
+import { IsDate, IsEnum, IsNumber, IsString } from 'class-validator';
 import { College } from 'src/app/student/enum/college.enum';
 import { Course } from 'src/app/student/enum/course.enum';
 import { Faculty } from 'src/app/student/enum/faculty.enum';
@@ -7,7 +7,6 @@ import { TransportStatus } from 'src/app/transport/enum/transport-status.enum';
 import { TransportType } from 'src/app/transport/enum/transport-type.enum';
 import { ReportStatus } from '../enum/report-status.enum';
 import { ReportEntity } from '../repository/report.entity';
-import { TrafficErrorDto } from './traffic-error.dto';
 
 export class ReportDto {
     @ApiProperty({ description: 'Report ID', example: 1 })
@@ -15,10 +14,10 @@ export class ReportDto {
     id: number;
 
     @ApiProperty({
-        description: 'Report status',
+        description: 'Report status key',
         example: ReportStatus.New,
     })
-    @IsString()
+    @IsEnum(ReportStatus)
     status: string;
 
     @ApiProperty({ description: 'Report location', example: 'UiTM' })
@@ -47,17 +46,17 @@ export class ReportDto {
     remark: string;
 
     @ApiProperty({
-        description: 'Transport status',
+        description: 'Transport status key',
         example: TransportStatus.Locked,
     })
-    @IsString()
+    @IsEnum(TransportStatus)
     transportStatus: string;
 
     @ApiProperty({
-        description: 'Transport type',
+        description: 'Transport type key',
         example: TransportType.Car,
     })
-    @IsString()
+    @IsEnum(TransportType)
     transportType: string;
 
     @ApiProperty({
@@ -89,21 +88,21 @@ export class ReportDto {
     studentFullname: string;
 
     @ApiProperty({
-        description: 'Student course',
+        description: 'Student course key',
         example: Course.Cs230,
     })
-    @IsString()
+    @IsEnum(Course)
     studentCourse: string;
 
     @ApiProperty({
-        description: 'Student faculty',
+        description: 'Student faculty key',
         example: Faculty.Fskm,
     })
-    @IsString()
+    @IsEnum(Faculty)
     studentFaculty: string;
 
     @ApiProperty({
-        description: 'Student college',
+        description: 'Student college key',
         example: College.Delima,
     })
     @IsString()
@@ -111,20 +110,15 @@ export class ReportDto {
 
     @ApiProperty({
         description: 'Error list',
-        example: [
-            {
-                name: 'e1',
-                description: 'Menghalang laluan',
-            },
-        ],
+        example: ['e1', 'e2'],
     })
-    @IsString()
-    trafficErrors: TrafficErrorDto[];
+    @IsString({ each: true })
+    trafficErrors: string[];
 
     static fromModel(model: ReportEntity): ReportDto {
         return {
             id: model?.id,
-            status: model?.reportStatus?.description,
+            status: model?.reportStatus?.name,
             location: model?.location,
             createdAt: model?.createdAt,
             updatedAt: model?.updatedAt,
@@ -133,30 +127,20 @@ export class ReportDto {
                     ? model?.reportHistories[model?.reportHistories.length - 1]
                           ?.remark
                     : null,
-            transportStatus:
-                model?.reportTransport?.transportStatus?.description,
-            transportType: model?.reportTransport?.transportType?.description,
+            transportStatus: model?.reportTransport?.transportStatus?.name,
+            transportType: model?.reportTransport?.transportType?.name,
             transportPlateNo: model?.reportTransport?.plateNo,
             transportPassCode: model?.reportTransport?.passCode,
             studentCode: model?.reportStudent?.studentCode,
             studentFullname: model?.reportStudent?.fullname,
-            studentCourse: model?.reportStudent?.studentCourse?.description,
+            studentCourse: model?.reportStudent?.studentCourse?.name,
             studentFaculty:
-                model?.reportStudent?.studentCourse?.courseFaculty?.description,
-            studentCollege: model?.reportStudent?.studentCollege?.description,
+                model?.reportStudent?.studentCourse?.courseFaculty?.name,
+            studentCollege: model?.reportStudent?.studentCollege?.name,
             trafficErrors: model?.reportReportTrafficErrors
                 ? model?.reportReportTrafficErrors?.map(
                       (error: object) =>
-                          ({
-                              name:
-                                  error['reportTrafficErrorTrafficError'][
-                                      'name'
-                                  ],
-                              description:
-                                  error['reportTrafficErrorTrafficError'][
-                                      'description'
-                                  ],
-                          } as TrafficErrorDto),
+                          error['reportTrafficErrorTrafficError']['name'],
                   )
                 : [],
         } as ReportDto;
