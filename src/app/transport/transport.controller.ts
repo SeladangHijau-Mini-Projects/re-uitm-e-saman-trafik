14c9925c1e86bc7f98e4fbda05059d9372c432f7
Muhammad Nadzmi Bin Mohamed Idzham <nadzmi.idzham@carsome.com>
2021-05-19 00:03:34 +0800

    finished get multiple transport endpoint

--- src/app/transport/transport.controller.ts
+++ src/app/transport/transport.controller.ts
@@ -1,23 +1,30 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { TransportDetailDto } from './dto/transport-detail.dto';
+import { TransportQueryParamDto } from './dto/transport-query-param.dto';
+import { TransportSummaryDto } from './dto/transport-summary.dto';
 import { TransportService } from './transport.service';
 
 @Controller()
 export class TransportController {
     constructor(private readonly transportService: TransportService) {}
 
-    // TODO: get multiple transport endpoint
-    // @Get()
-    // @ApiOperation({ summary: 'Get multiple transport.' })
-    // @ApiResponse({
-    //     status: 200,
-    //     description: 'Success',
-    //     type: [TransportSummaryDto],
-    // })
-    // @ApiResponse({ status: 401, description: 'Unauthorized.' })
-    // @ApiResponse({ status: 500, description: 'Internal Server Error.' })
-    // async findAll(): Promise<TransportSummaryDto> {}
+    @Get()
+    @ApiOperation({ summary: 'Get multiple transport.' })
+    @ApiResponse({
+        status: 200,
+        description: 'Success',
+        type: [TransportSummaryDto],
+    })
+    @ApiResponse({ status: 401, description: 'Unauthorized.' })
+    @ApiResponse({ status: 500, description: 'Internal Server Error.' })
+    async findAll(
+        @Query() param: TransportQueryParamDto,
+    ): Promise<TransportSummaryDto[]> {
+        return (await this.transportService.findAll(param)).map(
+            TransportSummaryDto.fromModel,
+        );
+    }
 
     @Get(':transportId')
     @ApiOperation({ summary: 'Get single transport.' })
