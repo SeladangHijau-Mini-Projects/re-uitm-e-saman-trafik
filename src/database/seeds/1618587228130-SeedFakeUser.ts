3a048fa00e870aff6f2ff22489c9db1e8469302d
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2021-08-10 17:23:50 +0800

    finished seed migration scrrpt

--- src/database/seeds/1618587228130-SeedFakeUser.ts
+++ src/database/seeds/1618587228130-SeedFakeUser.ts
@@ -2,15 +2,16 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class SeedFakeUser1618587228130 implements MigrationInterface {
     async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE users AUTO_INCREMENT = 1`);
         await queryRunner.query(
             `
                 INSERT INTO users(
-                    rank_id, type_id, user_code,
-                    fullname, phone_tel_no, office_tel_no,
+                    type_id, user_code,
+                    name, mobile_tel_no, office_tel_no,
                     first_timer, email
                 ) 
                 VALUES (
-                    1, 1, 'A380',
+                    1, 'A380',
                     'Nadzmi Admin', '01110849181', '044911376',
                     1, 'nadzmiidzham@gmail.com'
                 );
@@ -19,12 +20,12 @@ export class SeedFakeUser1618587228130 implements MigrationInterface {
         await queryRunner.query(
             `
                 INSERT INTO users(
-                    rank_id, type_id, user_code,
-                    fullname, phone_tel_no, office_tel_no,
+                    type_id, user_code,
+                    name, mobile_tel_no, office_tel_no,
                     first_timer, email
                 ) 
                 VALUES (
-                    1, 2, 'S380',
+                    2, 'S380',
                     'Nadzmi Staff', '01110849181', '044911376',
                     1, 'nadzmiidzham@gmail.com'
                 );
@@ -33,12 +34,12 @@ export class SeedFakeUser1618587228130 implements MigrationInterface {
         await queryRunner.query(
             `
                 INSERT INTO users(
-                    rank_id, type_id, user_code,
-                    fullname, phone_tel_no, office_tel_no,
+                    type_id, user_code,
+                    name, mobile_tel_no, office_tel_no,
                     first_timer, email
                 ) 
                 VALUES (
-                    1, 3, 'P380',
+                    3, 'P380',
                     'Nadzmi Police', '01110849181', '044911376',
                     1, 'nadzmiidzham@gmail.com'
                 );
